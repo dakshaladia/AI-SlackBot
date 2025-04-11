f8234df22b944e81e7c9ddf5300f28fbab9b60d9
Daksha Ladia <ddladia58@hotmail.com>
2025-04-11 19:36:37 -0400

    first initial commit

--- backend/models/slackModule.js
+++ backend/models/slackModule.js
@@ -1,23 +1,21 @@
 import { WebClient } from '@slack/web-api';
 
-// const slackToken = process.env.SLACK_BOT_TOKEN;
-const slackToken="xoxb-8721745496884-8730587885169-rBIb3rBOFeLbCs2FnHROUiaA"
+const slackToken = process.env.SLACK_BOT_TOKEN;
 
-// console.log('token:',slackToken); // Your Slack bot token
+// console.log('token:',slackToken); 
 const slackClient = new WebClient(slackToken);
 
 // Function to check Slack for a response
 export async function checkSlackForResponse(message) {
     try {
         const result = await slackClient.conversations.history({
-            channel: 'C08MG7L9LN5', // Your channel ID
+            channel: 'C08MG7L9LN5', // Enter your channelID
             limit: 10, // Number of messages to retrieve
         });
 
-        // Gather the text of the messages
+       
         const messagesText = result.messages.map(msg => msg.text).join('\n');
 
-        // Return the concatenated messages for AI processing
         return messagesText; // Return all messages as a single string
     } catch (error) {
         console.error('Error fetching messages from Slack:', error);
